b1a23265b21e7cfe47bbd20f77d8eb3d2e0e3627
Vy Nguyen <vydpnguyen@gmail.com>
2022-11-02 14:26:18 -0700

    Update variables in Obstacle.java

--- src/main/java/TigerGame/Obstacle.java
+++ src/main/java/TigerGame/Obstacle.java
@@ -1,5 +1,21 @@
 package TigerGame;
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
+import java.awt.*;
+import java.awt.event.*;
+
 
 public class Obstacle {
-
+	private ObstacleType obstacleType;
+	private double posX;
+	private double posY;
+	private double moveSpeed;
+	private GImage obstacleImage;
+	private double obstacleWidth;
+	private double obstacleHeight;
+	//private Timer t;
+	
+	
+	
+	
 }
