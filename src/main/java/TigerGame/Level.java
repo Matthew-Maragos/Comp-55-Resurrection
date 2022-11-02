026c76fe21de73ee0a895d22da190591022a7edc
Ayden Cabral <a_cabral5@u.pacific.edu>
2022-11-02 14:25:17 -0700

    Added some functions and other various things.

--- src/main/java/TigerGame/Level.java
+++ src/main/java/TigerGame/Level.java
@@ -1,5 +1,41 @@
 package TigerGame;
 
-public class Level {
+import java.util.Timer;
+import acm.graphics.GImage;
 
+//
+//DO NOT PUSH SETTINGS, ONLY ADDED/MODIFIED
+//
+
+public class Level 
+{
+	GImage background = new GImage("robot.png");
+//	add(background, 290, 1);
+
+//	Timer levelTimer
+	
+	void addElement()
+	{
+		
+	}
+	
+	void addObstacle()
+	{
+		
+	}
+	
+	void addPowerUp()
+	{
+		
+	}
+	
+	void endGame()//(checkCollided())
+	{
+		
+	}
+	
+	void obstacleEncountered() 
+	{
+		
+	}
 }
