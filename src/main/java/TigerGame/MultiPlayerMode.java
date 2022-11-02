37b8551192665361f791f8c1b16e1936de2f17bd
Ayden Cabral <a_cabral5@u.pacific.edu>
2022-11-02 14:16:15 -0700

    Added some functions and other things.

--- src/main/java/TigerGame/MultiPlayerMode.java
+++ src/main/java/TigerGame/MultiPlayerMode.java
@@ -1,17 +1,30 @@
 package TigerGame;
+
+import java.util.ArrayList;
 //
 //	DO NOT PUSH SETTINGS, ONLY ADDED/MODIFIED
 //
 
 public class MultiPlayerMode 
 {
-//	Player PlayerOne
-//  Player PlayerTwo 
+//	Player PlayerOne;
+//  Player PlayerTwo; 
 
-//	Level gameLevelOne
-//	Level gameLevelOne 
-
+//	Level gameLevelOne;
+//	Level gameLevelOne; 
+	ArrayList<String> Player = new ArrayList<String>();
 	
+//	Player.add("Vesena");
+		
+	void startGame()
+	{
+		
+	}
+	
+	void endGame()
+	{
+		
+	}
 
 	
 }
