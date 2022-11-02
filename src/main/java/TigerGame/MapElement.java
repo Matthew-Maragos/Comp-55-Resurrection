2eca3e3981e8d9016ca6f7b02fa6902fa5a2335c
Matthew-Maragos <m_maragos@u.pacific.edu>
2022-11-02 15:07:10 -0700

    Data members and getter/setter for MapElement class

--- src/main/java/TigerGame/MapElement.java
+++ src/main/java/TigerGame/MapElement.java
@@ -1,5 +1,38 @@
 package TigerGame;
 
+import java.util.Timer;
+import acm.graphics.GImage;
+
 public class MapElement {
+	
+	private MapElementType mapEleType;
+	private double posX, posY;
+	private int moveSpeed = 10;
+	GImage elementImage;
+	
+	
+	public double getPosX() {
+		return posX;
+	}
+
+	public void setPosX(double posX) {
+		this.posX = posX;
+	}
+
+	public double getPosY() {
+		return posY;
+	}
+
+	public void setPosY(double posY) {
+		this.posY = posY;
+	}
+
+	public MapElementType getMapEleType() {
+		return mapEleType;
+	}
+
+	public void setMapEleType(MapElementType mapEleType) {
+		this.mapEleType = mapEleType;
+	}
 
 }
