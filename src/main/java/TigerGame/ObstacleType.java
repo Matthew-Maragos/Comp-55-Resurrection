15f47c3046506e01285fff1c59c6ae7fabd13557
Vy Nguyen <vydpnguyen@gmail.com>
2022-11-02 14:07:00 -0700

    Add toString() and types in PowerUpType, ObstacleType and MapElementType

--- src/main/java/TigerGame/ObstacleType.java
+++ src/main/java/TigerGame/ObstacleType.java
@@ -2,5 +2,16 @@ package TigerGame;
 
 public enum ObstacleType {
  //notes I hope this works
-	ROCK, BENCH; 
+	TRASHCAN, ACORN, ROCK, BENCH; 
+	
+	public String toString() {
+		switch(this) {
+			case TRASHCAN: return "trashcan";
+			case ACORN: return "acorn";
+			case ROCK: return "rock";
+			case BENCH: return "bench";
+			
+		}
+		return "n/a";
+	}
 }
--- src/main/java/TigerGame/PowerUpType.java
+++ src/main/java/TigerGame/PowerUpType.java
@@ -1,5 +1,18 @@
 package TigerGame;
 
 public enum PowerUpType {
-
+	
+ONEUP, DOUBLEJUMP, INVINCIBILITY, JEOPARDY;
+	
+	public String toString() {
+		switch(this) {
+			case ONEUP: return "cloud";
+			case DOUBLEJUMP: return "doublejump";
+			case INVINCIBILITY: return "invincibility";
+			case JEOPARDY: return "jeopardy";
+			
+		}
+		return "n/a";
+	}
+	
 }
