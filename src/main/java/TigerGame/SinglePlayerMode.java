9453a425a842ad44643a55bd4a3c0749285549d5
Ayden Cabral <a_cabral5@u.pacific.edu>
2022-11-04 14:58:48 -0700

    Changed things, this is a test to push.

--- src/main/java/TigerGame/SinglePlayerMode.java
+++ src/main/java/TigerGame/SinglePlayerMode.java
@@ -1,5 +1,25 @@
 package TigerGame;
 
-public class SinglePlayerMode {
+import java.util.ArrayList;
 
+public class SinglePlayerMode 
+{
+	Player PlayerOne;
+	Player PlayerTwo; 
+
+	Level gameLevelOne;
+	Level gameLevelTwo; 
+//	ArrayList<GImage> Player = new ArrayList<GImage>();
+	
+//	Player.add("Vesena");
+		
+	void startGame()
+	{
+		
+	}
+	
+	void endGame()
+	{
+		
+	}
 }
