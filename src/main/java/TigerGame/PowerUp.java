a1ceee00b867c7903f69f43271d49620dd6862e4
clara <clara@10.11.145.154>
2022-11-04 14:18:09 -0700

    Added the getters and setters (just source generated)

--- src/main/java/TigerGame/PowerUp.java
+++ src/main/java/TigerGame/PowerUp.java
@@ -19,12 +19,45 @@ public class PowerUp {
 	public PowerUpType getPowerType (PowerUpType powerType) {
 		return this.powerType;
 	}
-	
+
 	//getters and setters for posX/Y and powerHeight/Width
+	
+	public double getPosX() {
+		return posX;
+	}
+
+	public void setPosX(double posX) {
+		this.posX = posX;
+	}
+
+	public double getPosY() {
+		return posY;
+	}
+
+	public void setPosY(double posY) {
+		this.posY = posY;
+	}
+
+	public double getPowerWidth() {
+		return powerWidth;
+	}
+
+	public void setPowerWidth(double powerWidth) {
+		this.powerWidth = powerWidth;
+	}
+	
+	public double getPowerHeight() {
+		return powerHeight;
+	}
+
+	public void setPowerHeight(double powerHeight) {
+		this.powerHeight = powerHeight;
+	}
+	
 	
 	//need an activate PowerUp class
 	
-	//move() function move to level function this will also get rid of timer
+	//move() function move to level function this will also get rid of timer probably a boolean(?)
 	
 	
 	
