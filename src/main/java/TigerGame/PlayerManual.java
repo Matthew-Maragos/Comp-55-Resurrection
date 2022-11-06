3743951a681db4fcf68cd307440416751f8068e6
Matthew-Maragos <m_maragos@u.pacific.edu>
2022-11-06 13:34:01 -0800

    Added getArrowHeight function, but nothing inside. Also commented all the functions.

--- src/main/java/TigerGame/PlayerManual.java
+++ src/main/java/TigerGame/PlayerManual.java
@@ -10,7 +10,7 @@ public class PlayerManual {
 	GImage manualImage;
 	GImage arrowImage;
 	
-/**/	// Adding from the Diagram, but needs more work
+/*	// Adding from the Diagram, but needs more work
 	
 	public void mouseClicked(MouseEvent e) {
 		
@@ -19,5 +19,9 @@ public class PlayerManual {
 	double getArrowWidth() {
 		return 0;
 	}
-
+	
+	double getArrowHeight() {
+		return 0;
+	}
+*/
 }
