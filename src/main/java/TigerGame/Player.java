f57cfbdbbc2e98006d198725c302213889631d95
Matthew-Maragos <m_maragos@u.pacific.edu>
2022-11-06 13:41:55 -0800

    Added getter and setter for posY in Player class.

--- src/main/java/TigerGame/Player.java
+++ src/main/java/TigerGame/Player.java
@@ -19,5 +19,12 @@ public class Player {
 	public void setPosX(double posX) {
 		this.posX = posX;
 	}
+	public double getPosY() {
+		return posY;
+	}
+	public void setPosY(double posY) {
+		this.posY = posY;
+	}
+	
 	
 }
