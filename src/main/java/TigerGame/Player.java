d1faa39242c006c2e8351510a17a936602349dc5
Matthew-Maragos <m_maragos@u.pacific.edu>
2022-11-06 13:39:26 -0800

    Added all the variables from the diagram into Player class.

--- src/main/java/TigerGame/Player.java
+++ src/main/java/TigerGame/Player.java
@@ -1,6 +1,16 @@
 package TigerGame;
 
+import acm.graphics.GImage;
+
 public class Player {
-	
+	private double moveSpeed;
+	private double posX;
+	private double posY;
+	private double tigerWidth;
+	private double tigerHeight;
+	private double playerScore;
+	GImage tigerRunImage;
+	GImage tigerJumpImage;
+	GImage tigerLandImage;
 	
 }
