3d6b62a0c2bb7b7421912c65987d23ae40590014
Ayden Cabral <ayden8578@aol.com>
2022-11-11 14:46:28 -0800

    Did some cleaning and added some varibales

--- src/main/java/TigerGame/PlayerManual.java
+++ src/main/java/TigerGame/PlayerManual.java
@@ -4,24 +4,25 @@ package TigerGame;
 import java.awt.event.MouseEvent;
 import acm.graphics.GImage;
  
-public class PlayerManual {
+public class PlayerManual 
+{
 
-	GImage titleImage;
-	GImage manualImage;
-	GImage arrowImage;
-	
-/*	// Adding from the Diagram, but needs more work
-	
-	public void mouseClicked(MouseEvent e) {
-		
-	}
-	
-	double getArrowWidth() {
-		return 0;
-	}
-	
-	double getArrowHeight() {
-		return 0;
-	}
-*/
+    private double ArrowWidth;
+    private double ArrowHeight;
+
+    GImage titleImage;
+    GImage manualImage;
+    GImage arrowImage;
+
+    double getArrowWidth() 
+    {
+        return ArrowWidth;
+    }
+
+    double getArrowHeight() 
+    {
+        return ArrowHeight;
+    }
 }
+
+    //Added function TODO something cool
