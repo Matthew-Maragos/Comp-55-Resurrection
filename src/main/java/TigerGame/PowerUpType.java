5c6f97bc6d9bfb492a0e162627f7540effe88422
Matthew Maragos <m_maragos@u.pacific.edu>
2024-01-29 20:09:24 -0800

    Added new PowerUpType named EXTRAPOINTS.

--- src/main/java/TigerGame/PowerUpType.java
+++ src/main/java/TigerGame/PowerUpType.java
@@ -2,13 +2,14 @@ package TigerGame;
 
 public enum PowerUpType {
 	
-ONEUP, DOUBLEJUMP, INVINCIBILITY, JEOPARDY;
+ONEUP, DOUBLEJUMP, INVINCIBILITY, JEOPARDY, EXTRAPOINTS;
 	
 	public String toString() {
 		switch(this) {
 			case ONEUP: return "oneup";
 			case DOUBLEJUMP: return "doublejump";
 			case INVINCIBILITY: return "invincibility";
+			case EXTRAPOINTS: return "extrapoints";
 		}
 		return "n/a";
 	}
